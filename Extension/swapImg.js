8013d328edcb59c8bd43e3d4dc4871d49e92a217
Ari Blondal <ari.blondal@gmail.com>
2020-11-07 19:57:42 -0800

    test

--- Extension/swapImg.js
+++ Extension/swapImg.js
@@ -14,20 +14,20 @@ for (var i = 0; i < images.length; i++) {
     fetch("https://localhost:5000/", { method: 'POST', body: JSON.stringify({ img: my_data }), header: { "Access-Control-Allow-Origin": "self" }, credentials: "same-origin" }).then(res => res.json()).then(function (data) { images[i].src = data['img'] }).catch(err => { console.log(err) })
 }
 
-var images = document.getElementsByTagName('img');
-console.log(images)
-for(var i = 0; i < images.length; i++) {
-	//TODO create json of image, get response img, change byte64
-	fetch('https://localhost:5000/', {
-		method: 'POST',
-		body: JSON.stringify({img:images[i].src}),
-		header:{"Access-Control-Allow-Origin":"*"}
-	}).then(
-		res=>res.text()
-	).then(function(data){
-		images[i].src=data
-	}).catch(
-		err=>{console.log(err)}
-	)
-}
+// var images = document.getElementsByTagName('img');
+// console.log(images)
+// for(var i = 0; i < images.length; i++) {
+// 	//TODO create json of image, get response img, change byte64
+// 	fetch('https://localhost:5000/', {
+// 		method: 'POST',
+// 		body: JSON.stringify({img:images[i].src}),
+// 		header:{"Access-Control-Allow-Origin":"*"}
+// 	}).then(
+// 		res=>res.text()
+// 	).then(function(data){
+// 		images[i].src=data
+// 	}).catch(
+// 		err=>{console.log(err)}
+// 	)
+// }
 
